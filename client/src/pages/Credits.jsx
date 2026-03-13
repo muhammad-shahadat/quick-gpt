c6cdbc8ed8ef297b9294284a5f9323db684a7e3e
muhammad-shahadat <shahadat6640@gmail.com>
2026-03-13 15:56:01 +0600

    update buy now button logic in credits page

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -12,7 +12,7 @@ const Credits = () => {
 
     const { data, isLoading, isError, error } = useGetPlans();
     const plans = data;
-    const { mutate: createCheckout, isPending: isRedirecting } = usePaymentMutations();
+    const { mutate: createCheckout, isPending, variables } = usePaymentMutations();
 
     if (isLoading) return <Loading />
 
@@ -22,39 +22,47 @@ const Credits = () => {
 
             <div className='flex flex-wrap justify-center gap-8'>
                 {
-                    plans.map((plan) => (
-                        <div key={plan._id} className={`border border-gray-200 dark:border-purple-700 rounded-lg shadow hover:shadow-lg transition-shadow p-6 min-w[300px] flex flex-col ${plan.slug === "premium" ? "bg-purple-50 dark:bg-purple-900" : "bg-white dark:bg-transparent"}`}>
-                            <div className='flex-1'>
-                                <h3 className='text-xl font-semibold text-gray-900 dark:text-white mb-2'>{plan.name}</h3>
-                                <p className='text-2xl font-bold text-purple-600 dark:text-purple-300 mb-4'>
-                                    ${plan.price}
-                                    <span className='text-base font-normal text-gray-600 dark:text-purple-200'> / {plan.credits} credits</span>
+                    plans.map((plan) => {
 
-                                </p>
-                                <ul className='list-disc list-inside text-sm text-gray-700 dark:text-purple-200 space-y-1'>
-                                    {
-                                        plan.features.map((feature, index) => (
-                                            <li key={index}>{feature}</li>
-                                        ))
-                                    }
-                                </ul>
+                        const isThisPlanLoading = isPending && variables === plan._id;
 
+                        return (
+
+                            <div key={plan._id} className={`border border-gray-200 dark:border-purple-700 rounded-lg shadow hover:shadow-lg transition-shadow p-6 min-w[300px] flex flex-col ${plan.slug === "premium" ? "bg-purple-50 dark:bg-purple-900" : "bg-white dark:bg-transparent"}`}>
+                                <div className='flex-1'>
+                                    <h3 className='text-xl font-semibold text-gray-900 dark:text-white mb-2'>{plan.name}</h3>
+                                    <p className='text-2xl font-bold text-purple-600 dark:text-purple-300 mb-4'>
+                                        ${plan.price}
+                                        <span className='text-base font-normal text-gray-600 dark:text-purple-200'> / {plan.credits} credits</span>
+
+                                    </p>
+                                    <ul className='list-disc list-inside text-sm text-gray-700 dark:text-purple-200 space-y-1'>
+                                        {
+                                            plan.features.map((feature, index) => (
+                                                <li key={index}>{feature}</li>
+                                            ))
+                                        }
+                                    </ul>
+
+                                </div>
+                                <button
+                                    disabled={isPending}
+                                    onClick={() => createCheckout(plan._id)}
+                                    className='mt-6 bg-purple-600 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer'
+                                >
+                                    {isThisPlanLoading ? (
+                                        <div className='flex items-center justify-center'><Loader2 className="w-5 h-5 animate-spin" /></div>
+                                    ) : (
+
+                                        "Buy Now"
+                                    )}
+
+                                </button>
                             </div>
-                            <button
-                                disabled={isRedirecting}
-                                onClick={() => createCheckout(plan._id)}
-                                className='mt-6 bg-purple-600 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer'
-                            >
-                                {isRedirecting ? (
-                                    <Loader2 className="w-5 h-5 animate-spin" />
-                                ) : (
+                        )
 
-                                    "Buy Now"
-                                )}
-
-                            </button>
-                        </div>
-                    ))
+                    })
+                        
                 }
 
             </div>
