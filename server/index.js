948cf59421a6417a725ca28e410fbd5180c01d89
muhammad-shahadat <shahadat6640@gmail.com>
2026-06-13 09:20:14 +0600

    add postman collection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import "dotenv/config"
+
 import app from "./src/app.js";
 import { connectDB } from "./src/configs/db.js";
+import { seedPlans } from "./src/configs/seeds/planSeed.js";
 
 
 
@@ -9,18 +11,20 @@ const port = process.env.PORT || 4000;
 
 (async () => {
 
-   try {
+    try {
 
-      await connectDB();
-      app.listen(port, () =>{  
-         console.log(`server is running at http://localhost:${port}`);
+        await connectDB();
+        // ২. কানেক্ট হওয়ার পরেই সিডার চেক করবে এবং ডেটা না থাকলে ইনসার্ট করবে
+        await seedPlans();
+        app.listen(port, () => {
+            console.log(`server is running at http://localhost:${port}`);
 
-      })
-      
-   } catch (error) {
-      console.error(`Server start error: ${error.message}`);
-      
-   }
+        })
+
+    } catch (error) {
+        console.error(`Server start error: ${error.message}`);
+
+    }
 
 })()
 
